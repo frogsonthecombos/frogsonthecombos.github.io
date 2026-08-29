80085078681292c1b8ea600beedc2816fd0602bd
frogsonthecombos <112841049+frogsonthecombos@users.noreply.github.com>
2026-08-29 18:00:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-function changeMessage() {
+const messages = [
+    "🐸 Ribbit!",
+    "You clicked the frog button!",
+    "Welcome to the combo zone!",
+    "This website actually works!",
+    "🐸🐸🐸 MORE FROGS!",
+    "You have discovered the secret.",
+    "The frog approves."
+];
 
-    const messages = [
-        "🐸 Ribbit!",
-        "You clicked the frog button!",
-        "Welcome to the combo zone!",
-        "This website actually works!",
-        "🐸🐸🐸 MORE FROGS!",
-        "You have discovered the secret.",
-        "The frog approves."
-    ];
+const button = document.getElementById("frogButton");
+const message = document.getElementById("message");
 
+button.addEventListener("click", function () {
     const randomMessage =
         messages[Math.floor(Math.random() * messages.length)];
 
-    document.getElementById("message").textContent = randomMessage;
-}
-
+    message.textContent = randomMessage;
+});
