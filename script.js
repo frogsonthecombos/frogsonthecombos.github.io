49a41638a3ddc99445a256db80748f075c169290
frogsonthecombos <112841049+frogsonthecombos@users.noreply.github.com>
2026-08-29 17:43:48 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeMessage() {
+    const messages = [
+        "🐸 Ribbit!",
+        "You clicked the frog button!",
+        "Welcome to the combo zone!",
+        "This website actually works!",
+        "🐸🐸🐸 MORE FROGS!",
+        "You have discovered the secret.",
+        "The frog approves."
+    ];
+
+    const randomMessage =
+        messages[Math.floor(Math.random() * messages.length)];
+
+    document.getElementById("message").textContent = randomMessage;
+}
